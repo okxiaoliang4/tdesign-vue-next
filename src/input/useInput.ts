e587be180e52bc5bf2d86827585eabe19aab3302
Jelf <okxiaoliang4@gmail.com>
2024-04-11 15:13:56 +0800

    chore: 还原

--- src/input/useInput.ts
+++ src/input/useInput.ts
@@ -133,7 +133,7 @@ export default function useInput(props: ExtendsTdInputProps, expose: (exposed: R
 
   const formItem = inject(FormItemInjectionKey, undefined);
   const formatAndEmitBlur = (e: FocusEvent) => {
-    // if (isHover.value) return;
+    if (isHover.value) return;
     if (!isClearIcon()) {
       if (props.format) {
         inputValue.value =
